55322149eb44a92367a1a1e022b3345fa3e195c6
Sofya <sofya.s.frolova@gmail.com>
2021-03-16 22:24:21 +0300

    First try

--- homework.py
+++ homework.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+import logging
 import requests
 import telegram
 from dotenv import load_dotenv
@@ -12,29 +12,51 @@ PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log', 
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    try:
+        homework_name = homework['homework_name']
+        current_homework_status = homework['status']
+    except KeyError:
+        logging.exception("Can't get homework status")
+        return 'Возникла ошибка'
+    if current_homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    elif current_homework_status == 'reviewing':
+        return f'Работу взяли на проверку'
+    elif current_homework_status == 'approved':
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    else:
+        logging.exception("Can't get homework status")
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {
+        'Authorization': f'OAuth {PRAKTIKUM_TOKEN}',
+    }
+    params ={
+        'from_date': current_timestamp
+    }
+    URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    try:
+        homework_statuses = requests.get(URL, headers=headers, params=params)
+    except requests.RequestException:
+        return {}
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
+    bot=telegram.Bot(token=TELEGRAM_TOKEN)# проинициализировать бота здесь
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
