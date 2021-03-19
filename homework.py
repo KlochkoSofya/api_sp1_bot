919c4527038effdb47d19d75b5739332824ecd81
Sofya <sofya.s.frolova@gmail.com>
2021-03-19 23:39:37 +0300

    after pep8

--- homework.py
+++ homework.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
@@ -27,17 +27,17 @@ def parse_homework_status(homework):
         logging.exception('Cant get homework status')
         return 'Возникла ошибка'
     homework_statuses = {
-        'rejected': f'У вас проверили работу "{homework_name}"!\n\n'\
-            f'К сожалению в работе нашлись ошибки.',
-        'approved': f'У вас проверили работу "{homework_name}"!\n\n'\
-            f'Ревьюеру всё понравилось, '\
-            f'можно приступать к следующему уроку.',
+        'rejected': f'У вас проверили работу "{homework_name}"!\n\n'
+                    f'К сожалению в работе нашлись ошибки.',
+        'approved': f'У вас проверили работу "{homework_name}"!\n\n'
+                    f'Ревьюеру всё понравилось, '
+                    f'можно приступать к следующему уроку.',
         'reviewing': 'Работу взяли на проверку'
     }
     if current_homework_status in homework_statuses:
         return f'{homework_statuses[current_homework_status]}'
     else:
-        logging.exception("Can't get homework status")
+        logging.exception('Cant get homework status')
 
 
 def get_homework_statuses(current_timestamp):
@@ -70,15 +70,15 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0]), bot)
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]), bot)
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            # time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            # time.sleep(5)
 
 
 if __name__ == '__main__':
